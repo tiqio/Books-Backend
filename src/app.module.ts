61a3b4b05d7cc4c035c1fdd915d7acf8ea2b820c
dd <dd@mixwan.net>
2025-01-11 22:02:53 +0800

    v1.0.2 选择使用goland开发后端

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestService } from './test.service';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { BookModule } from './modules/book/book.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+// TypeORM也以Module的形式被使用
 @Module({
-  imports: [],
+  // 完成模块接口的开发后需要在这里进行注册
+  imports: [
+    UserModule,
+    AuthModule,
+    BookModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'deplus',
+      database: 'vbenbook',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, TestService],
 })
