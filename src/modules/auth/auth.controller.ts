9ec1516833997237fb60658d0c07fae822dd1bc1
dd <dd@mixwan.net>
2025-01-11 23:17:23 +0800

    v1.0.5 通过AuthGuard和自定义注解实现login的简单放行和其他请求的拦截

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
   @Get('test')
   testStr() {
     return 'test';
+  }
+
+  @Public()
+  @Post('login')
+  login() {
+    return 'auth';
   }
 }
---
+++ src/modules/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './public.decorator';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private reflector: Reflector) {
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    console.log('isPublic', isPublic);
+    if (isPublic) {
+      return true;
+    }
+    return undefined;
+  }
+}
